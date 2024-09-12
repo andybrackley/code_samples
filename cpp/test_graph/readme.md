2b4fb9eeba55f08ee81c408238deb1ae4dbb8412
andybrackley <andybrackley@gmail.com>
2024-09-12 14:28:22 +0100

    Added in some sample code for SBE encode/decode

--- cpp/test_graph/readme.md
+++ cpp/test_graph/readme.md
@@ -1,3 +1,12 @@
+## SBE
+
+Tool downloaded from here: https://nuget.info/packages/sbe-tool/1.23.1.1
+
+D:\code_samples\cpp\test_graph>java -Dsbe.target.language=cpp -jar sbe-tool-all.jar src/proto/deribit_multicast.xml
+
+
 ## SBEPP
 
 [SBEPP Home](https://oleksandrkvl.github.io/sbepp/1.4.0/index.html)
+
+
--- cpp/test_graph/src/main.cpp
+++ cpp/test_graph/src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <array>
 
+/*
+#include "sbe.h"
 #include <sbepp/sbepp.hpp>
 #include "generated/deribit/deribit_multicast/messages/book.hpp"
 
@@ -20,9 +23,52 @@ void dumpBuffer(const std::array<char, 256> buf) {
     std::cout << "changeid: " << m.changeId().value() << " inst id: " << m.instrumentId().value() << std::endl;
 }
 
-int main(int, char**){
+void runTestSbepp() {
     auto buf = getEncoded();
     dumpBuffer(buf);
+}
+*/
 
+#include "generated/com_deribit_multicast/Book.h"
+
+using namespace com::deribit::multicast;
+
+std::array<char, 256> encodeSbe() {
+    std::array<char, 256> buf{};
+
+    MessageHeader hdr = MessageHeader();
+    hdr.wrap(buf.data(), 0, 0, buf.size())
+        .blockLength(Book::sbeBlockLength())
+        .templateId(Book::sbeTemplateId())
+        .schemaId(Book::sbeSchemaId())
+        .version(Book::sbeSchemaVersion());
+
+    Book b;
+    b.wrapForEncode(buf.data(), hdr.encodedLength(), buf.size() - hdr.encodedLength())
+        .changeId(1)
+        .instrumentId(5);
+
+    return buf;
+ }
+
+void dumpEncoded(std::array<char, 256>& buf) {
+    MessageHeader hdr = MessageHeader();
+
+    hdr.wrap(buf.data(), 0, 0, buf.size());
+    int actingVersion = hdr.version();
+    int actingBlockLength = hdr.blockLength();
+
+    Book b;
+    b.wrapForDecode(buf.data(), hdr.encodedLength(), actingBlockLength, actingVersion, buf.size());
+
+    std::cout << "changeid: " << b.changeId() << " inst id: " << b.instrumentId() << std::endl;
+
+}
+
+int main(int, char**){
+
+    auto buf = encodeSbe();
+    dumpEncoded(buf);
+    
     std::cout << "Hello, from test_graph!\n";
 }
